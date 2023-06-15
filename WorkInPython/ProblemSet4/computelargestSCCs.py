db8109f08fc5a130312fd1cd5f8b68b804d1fd50
kurealnum <cattledoger@gmail.com>
2023-06-15 13:47:25 -0400

    file load implemented in P4

---
+++ WorkInPython/ProblemSet4/computelargestSCCs.py
@@ -0,0 +1,14 @@
+#do not print the graph, its 5 million lines in a .txt so...
+graph = {}
+
+#reading the file
+with open('test.txt', "r") as f:
+    data = f.readlines()
+    for row in data:
+        row = row.strip()
+        row = row.split()
+        row = [int(x) for x in row]
+
+        vertex_outs = row[1:]
+        graph[row[0]] = vertex_outs + graph.get(row[0], [])
+

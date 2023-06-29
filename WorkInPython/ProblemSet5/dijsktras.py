1c0b765f5aaf87340d472c4fa685cbfdf59b05e8
kurealnum <oscar.gaske.cs@gmail.com>
2023-06-29 09:41:02 -0400

    first com for dijkstras

---
+++ WorkInPython/ProblemSet5/dijsktras.py
@@ -0,0 +1,11 @@
+#define an infinte variable
+import math
+inf = math.inf
+
+#g = graph
+#key: [length from key, adj veretex, distance]
+
+g = {
+    1: [2,3, inf] [4,2, inf],
+    2: [1,3, inf] []
+}
